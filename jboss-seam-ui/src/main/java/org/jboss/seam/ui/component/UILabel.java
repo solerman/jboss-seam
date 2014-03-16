21d9762eedd438462e6dd8619a029bbb5fd21470
Marek Novotný <hotmana76@gmail.com>
2014-03-16 16:54:10 +0100

    https://issues.jboss.org/browse/JBSEAM-5138

--- jboss-seam-ui/src/main/java/org/jboss/seam/ui/component/UILabel.java
+++ jboss-seam-ui/src/main/java/org/jboss/seam/ui/component/UILabel.java
@@ -18,7 +18,7 @@ import org.richfaces.cdk.annotations.Tag;
 @JsfComponent(description=@Description(displayName="org.jboss.seam.ui.Label",value="A label associated with the nearest JSF input component."),
 family="javax.faces.Output", type="org.jboss.seam.ui.Label",generate="org.jboss.seam.ui.component.html.HtmlLabel", 
 tag = @Tag(baseClass="org.jboss.seam.ui.util.cdk.UIComponentTagBase", name="label"), 
-attributes = {"javax.faces.component.UIOutput.xml", "accesskey-props.xml", "focus-props.xml" })
+attributes = {"javax.faces.component.UIOutput.xml", "core-props.xml","accesskey-props.xml", "focus-props.xml" })
 public abstract class UILabel extends HtmlOutputLabel implements UIDecorateAware
 {
    protected UIDecorate decorate;
